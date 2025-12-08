8a772169908152a09d7469e4f962e99893051b9e
AmirkhanGMs <amir_75817@gmail.com>
2025-12-08 13:54:32 +0500

    Fix: навигация и падения приложения, обновлен MainActivity, layouts, HomeFragment

---
+++ app/src/main/java/kz/itzhiti/donernaabaya/BasketFragment.kt
@@ -0,0 +1,6 @@
+package kz.itzhiti.donernaabaya
+
+import androidx.fragment.app.Fragment
+
+class BasketFragment : Fragment(R.layout.activity_basket_item) {
+}
---
+++ app/src/main/java/kz/itzhiti/donernaabaya/HomeFragment.kt
@@ -0,0 +1,28 @@
+package kz.itzhiti.donernaabaya
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.google.android.material.button.MaterialButton   // <-- ВАЖНО
+
+class HomeFragment : Fragment(R.layout.activity_home_fragment) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // КНОПКА КОРЗИНЫ
+        val basketButton = view.findViewById<MaterialButton>(R.id.btnBasket)
+        basketButton.setOnClickListener {
+            findNavController().navigate(R.id.basketFragment)
+            // или R.id.action_homeFragment_to_basketFragment, если так называется action
+        }
+
+        // КНОПКА ОТКРЫТЬ ДЕТАЛИ
+        val openDetailsButton = view.findViewById<MaterialButton>(R.id.btnOpenDetails)
+        openDetailsButton.setOnClickListener {
+            findNavController().navigate(R.id.itemDetailFragment)
+            // или action_homeFragment_to_itemDetailFragment
+        }
+    }
+}
