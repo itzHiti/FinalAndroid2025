3d48f71b882344ba571d518b0d3f367e72df1c2c
Daniyal551 <d_khaljanov@kbtu.kz>
2025-12-09 10:23:07 +0500

    Fix: навигация и падения приложения, обновлен MainActivity, layouts, HomeFragment

---
+++ app/src/main/java/kz/itzhiti/donernaabaya/ItemDetailFragment.kt
@@ -0,0 +1,28 @@
+package kz.itzhiti.donernaabaya
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ImageButton
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+
+class ItemDetailFragment : Fragment(R.layout.activity_item_detail) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // кнопка "В корзину" на экране детали
+        val addToBasketButton = view.findViewById<Button>(R.id.btnAtb)
+        addToBasketButton.setOnClickListener {
+            findNavController().navigate(
+                R.id.action_itemDetailFragment_to_basketFragment
+            )
+        }
+
+        val closeButton = view.findViewById<ImageButton>(R.id.btnClose)
+        closeButton.setOnClickListener {
+            findNavController().navigateUp()   // ← вернёт обратно на HomeFragment
+        }
+    }
+}
